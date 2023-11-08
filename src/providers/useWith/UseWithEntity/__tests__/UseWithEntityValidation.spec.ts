d11e33ce45614c71c17f47366b896c1082a965c8
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-11-07 21:32:39 -0800

    use with entity validation

--- src/providers/useWith/UseWithEntity/__tests__/UseWithEntityValidation.spec.ts
+++ src/providers/useWith/UseWithEntity/__tests__/UseWithEntityValidation.spec.ts
@@ -3,7 +3,7 @@ import { IItem } from "@entities/ModuleInventory/ItemModel";
 import { INPC } from "@entities/ModuleNPC/NPCModel";
 import { blueprintManager, container, unitTestHelper } from "@providers/inversify/container";
 import { IMagicItemUseWithEntity } from "@providers/useWith/useWithTypes";
-import { MagicsBlueprint } from "@rpg-engine/shared";
+import { EntityType, MagicsBlueprint } from "@rpg-engine/shared";
 import { UseWithEntityValidation } from "../UseWithEntityValidation";
 
 describe("UseWithEntityValidation", () => {
@@ -73,6 +73,61 @@ describe("UseWithEntityValidation", () => {
       const result = useWithEntityValidation.baseValidation(testCharacter, testItem, testItemBlueprint, "Item");
 
       expect(result).toBe(true);
+    });
+  });
+
+  describe("Target validation", () => {
+    it("returns false when the target is not found", async () => {
+      const result = await useWithEntityValidation.validateTargetRequest(
+        testCharacter,
+        null, // no target
+        testItem,
+        testItemBlueprint,
+        "Item"
+      );
+      expect(result).toBe(false);
+    });
+
+    it("returns false when the target is not in the same scene as the caster", async () => {
+      testNPC.scene = "DifferentScene"; // Assume 'testCharacter.scene' is "Scene1"
+      const result = await useWithEntityValidation.validateTargetRequest(
+        testCharacter,
+        testNPC, // NPC is in a different scene
+        testItem,
+        testItemBlueprint,
+        "Item"
+      );
+      expect(result).toBe(false);
+    });
+
+    it("returns false when using Heal Rune on an NPC", async () => {
+      const healRuneBlueprint = {
+        ...testItemBlueprint,
+        key: MagicsBlueprint.HealRune,
+        name: "Heal Rune",
+      } as IMagicItemUseWithEntity;
+      testNPC.type = EntityType.NPC;
+      const result = await useWithEntityValidation.validateTargetRequest(
+        testCharacter,
+        testNPC,
+        testItem,
+        healRuneBlueprint,
+        EntityType.NPC
+      );
+      expect(result).toBe(false);
+    });
+
+    it("returns false when the target is invisible", async () => {
+      // @ts-ignore
+      jest.spyOn(useWithEntityValidation.specialEffect, "isInvisible").mockResolvedValueOnce(true);
+      const result = await useWithEntityValidation.validateTargetRequest(
+        testCharacter,
+        testCharacter, // Assume the target character is the caster
+        testItem,
+        testItemBlueprint,
+        EntityType.Character
+      );
+      expect(result).toBe(false);
     });
   });
 });
