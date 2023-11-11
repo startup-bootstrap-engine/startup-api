d802b22e8c9fab5ff2aefa645b62d43e91345981
Joao Paulo Furtado <joaopaulofurtado@live.com>
2023-11-10 21:46:53 -0800

    pa constants fix

--- src/providers/constants/PremiumAccountConstants.ts
+++ src/providers/constants/PremiumAccountConstants.ts
@@ -1,3 +1,5 @@
+import { UserAccountTypes } from "@providers/user/userTypes";
+
 export interface IPremiumAccountRatio {
   SPXPLostOnDeathRatio: number;
   InventoryLossOnDeathRatio: number;
@@ -8,13 +10,13 @@ export interface IPremiumAccountRatio {
 }
 
 export interface IPremiumAccountData {
-  [AccountTypes.PremiumBronze]: IPremiumAccountRatio;
-  [AccountTypes.PremiumSilver]: IPremiumAccountRatio;
-  [AccountTypes.PremiumGold]: IPremiumAccountRatio;
+  [UserAccountTypes.PremiumBronze]: IPremiumAccountRatio;
+  [UserAccountTypes.PremiumSilver]: IPremiumAccountRatio;
+  [UserAccountTypes.PremiumGold]: IPremiumAccountRatio;
 }
 
 export const PREMIUM_ACCOUNT_TYPES_RATIOS = {
-  [AccountTypes.PremiumBronze]: {
+  [UserAccountTypes.PremiumBronze]: {
     SPXPLostOnDeathRatio: 1 - 0.2, // only loses 80% of the regular skill loss
     InventoryLossOnDeathRatio: 1 - 0.25, // 25% less chance to drop an item
     SpeedBuff: 7,
@@ -22,7 +24,7 @@ export const PREMIUM_ACCOUNT_TYPES_RATIOS = {
     lootDropBuff: 20,
     teleportCooldownMin: 40,
   },
-  [AccountTypes.PremiumSilver]: {
+  [UserAccountTypes.PremiumSilver]: {
     SPXPLostOnDeathRatio: 1 - 0.35, // only loses 60% of the regular skill loss
     InventoryLossOnDeathRatio: 1 - 0.5, // 50% less chance to drop an item
     SpeedBuff: 13,
@@ -30,7 +32,7 @@ export const PREMIUM_ACCOUNT_TYPES_RATIOS = {
     lootDropBuff: 30,
     teleportCooldownMin: 30,
   },
-  [AccountTypes.PremiumGold]: {
+  [UserAccountTypes.PremiumGold]: {
     SPXPLostOnDeathRatio: 1 - 0.5, // only loses 50% of the regular skill loss
     InventoryLossOnDeathRatio: 0, // 75% less chance to drop an item
     SpeedBuff: 15,
