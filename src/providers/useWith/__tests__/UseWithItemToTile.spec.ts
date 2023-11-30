d18938af638de9b12618f79307ec27c0b6bf0df4
João Paulo Furtado <30300459+jonit-dev@users.noreply.github.com>
2023-11-29 18:30:05 -0800

    RPG-2781-limit-resource-gathering-according-to-level (#899)
    * resource gathering constants
    
    * a condition to check the character level before using tile
    
    * formatting
    
    * test added
    
    * test case for all resources
    
    * lint fix
    
    * refactoring,fixed issues
    
    * refactoring,fixed issues
    
    ---------
    
    Co-authored-by: kal-kidan <kal05627356@gmail.com>

--- src/providers/useWith/__tests__/UseWithItemToTile.spec.ts
+++ src/providers/useWith/__tests__/UseWithItemToTile.spec.ts
@@ -1,57 +1,134 @@
-import { ICharacter } from "@entities/ModuleCharacter/CharacterModel";
+import { Character, ICharacter } from "@entities/ModuleCharacter/CharacterModel";
+import { Skill } from "@entities/ModuleCharacter/SkillsModel";
 import { container, unitTestHelper } from "@providers/inversify/container";
-import { CraftingResourcesBlueprint } from "@providers/item/data/types/itemsBlueprintTypes";
+import { CraftingResourcesBlueprint, FoodsBlueprint } from "@providers/item/data/types/itemsBlueprintTypes";
 import { SkillIncrease } from "@providers/skill/SkillIncrease";
 import { MapLayers } from "@rpg-engine/shared";
-import { IUseWithItemToTileOptions, UseWithItemToTile } from "../abstractions/UseWithItemToTile";
-
-jest.mock("lodash/random", () => jest.fn());
+import _ from "lodash";
+import { IUseWithItemToTileReward, UseWithItemToTile } from "../abstractions/UseWithItemToTile";
 
 describe("UseWithItemToTile.ts", () => {
   let useWithItemToTile: UseWithItemToTile;
   let skillIncrease: SkillIncrease;
-  let mockCharacter: ICharacter;
-  const baseOptions: IUseWithItemToTileOptions = {
-    rewards: [
-      {
-        chance: 20,
-        key: CraftingResourcesBlueprint.IronOre,
-        qty: [0, 3],
-      },
-      {
-        chance: 10,
-        key: CraftingResourcesBlueprint.GoldenOre,
-        qty: 3,
-      },
-    ],
-    targetTile: {
-      layer: MapLayers.Ground,
-      map: "test map",
-      x: 32,
-      y: 45,
-    },
-    errorAnimationEffectKey: "errorEffectKey",
-    errorMessages: ["error1", "error2", "error3"],
-    successAnimationEffectKey: "successEffectKey",
-    targetTileAnimationEffectKey: "targetTileEffectKey",
-    successMessages: ["success1", "success2", "success3"],
-    requiredResource: {
-      key: CraftingResourcesBlueprint.WaterBottle,
-      decrementQty: 1,
-      errorMessage: "you need to stay hydrated",
-    },
-  };
+  let testCharacter: ICharacter;
 
   beforeEach(async () => {
     useWithItemToTile = container.get<UseWithItemToTile>(UseWithItemToTile);
-    mockCharacter = await unitTestHelper.createMockCharacter(null, { hasEquipment: true, hasInventory: true });
+    testCharacter = await unitTestHelper.createMockCharacter(null, {
+      hasEquipment: true,
+      hasInventory: true,
+      hasSkills: true,
+    });
+    skillIncrease = container.get<SkillIncrease>(SkillIncrease);
+
+    await Skill.updateOne(
+      { owner: testCharacter._id },
+      {
+        $set: {
+          mining: {
+            level: 1,
+          },
+        },
+      }
+    );
   });
 
-  it("should make sure the character has the required items", async () => {});
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-  it("should decrement required resource and send the correct events", async () => {});
+  describe("Validation", () => {
+    it("should fail is required item is not available", async () => {
+      const decrementRequiredItem = jest.spyOn(
+        // @ts-ignore
+        useWithItemToTile.characterItemInventory,
+        "decrementItemFromInventoryByKey"
+      );
 
-  it("should add item to character inventory send the correct events", async () => {});
+      // @ts-ignore
+      const sendErrorMessageToCharacter = jest.spyOn(useWithItemToTile.socketMessaging, "sendErrorMessageToCharacter");
 
-  it("should add rewards sorted from rare to common", async () => {});
+      const result = await useWithItemToTile.execute(
+        testCharacter,
+        {
+          requiredResource: {
+            key: CraftingResourcesBlueprint.Worm,
+            decrementQty: 1,
+            errorMessage: "Sorry, you need a worm to fish.",
+          },
+          rewards: [
+            {
+              chance: 10,
+              key: FoodsBlueprint.BrownFish,
+              qty: 3,
+            },
+          ],
+          targetTile: {
+            layer: MapLayers.Ground,
+            map: "test map",
+            x: 32,
+            y: 45,
+          },
+        },
+        skillIncrease
+      );
+
+      expect(result).toBeUndefined();
+
+      expect(decrementRequiredItem).not.toHaveBeenCalled();
+
+      expect(sendErrorMessageToCharacter).toHaveBeenCalledWith(testCharacter, "Sorry, you need a worm to fish.");
+    });
+
+    it("should fail when trying to add reward to inventory, but has no required skill", async () => {
+      // mock lodash random to be 100
+
+      jest.spyOn(_, "random").mockImplementation(() => 100);
+
+      const mockRewards: IUseWithItemToTileReward[] = [
+        {
+          chance: 100,
+          key: CraftingResourcesBlueprint.GoldenOre,
+          qty: 3,
+        },
+      ];
+
+      // @ts-ignore
+      const result = await useWithItemToTile.addRewardToInventory(testCharacter, mockRewards);
+
+      expect(result).toBe(false);
+    });
+
+    it("should not fail if the reward meets the minimum required level", async () => {
+      await Skill.updateOne(
+        { owner: testCharacter._id },
+        {
+          $set: {
+            mining: {
+              level: 100,
+            },
+          },
+        }
+      );
+
+      const updatedCharacter = await Character.findById(testCharacter._id);
+
+      // mock lodash random to be 100
+
+      jest.spyOn(_, "random").mockImplementation(() => 100);
+
+      const mockRewards: IUseWithItemToTileReward[] = [
+        {
+          chance: 100,
+          key: CraftingResourcesBlueprint.GoldenOre,
+          qty: 3,
+        },
+      ];
+
+      // @ts-ignore
+      const result = await useWithItemToTile.addRewardToInventory(updatedCharacter, mockRewards);
+
+      expect(result).toBe(true);
+    });
+  });
 });
