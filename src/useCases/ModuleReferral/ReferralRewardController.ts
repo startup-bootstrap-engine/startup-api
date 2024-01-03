afb98ac0c2b956b42c04df5cabf01c75f84e7f99
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-01-02 19:08:27 -0800

    Update social crystal requirement and add referral bonus IP check

--- src/useCases/ModuleReferral/ReferralRewardController.ts
+++ src/useCases/ModuleReferral/ReferralRewardController.ts
@@ -1,22 +1,40 @@
 import { INPC } from "@entities/ModuleNPC/NPCModel";
+import { InMemoryHashTable } from "@providers/database/InMemoryHashTable";
 import { BadRequestError } from "@providers/errors/BadRequestError";
 import { HttpStatus } from "@rpg-engine/shared";
-import { controller, httpPost, interfaces, requestBody, response } from "inversify-express-utils";
+import { controller, httpPost, interfaces, request, requestBody, response } from "inversify-express-utils";
 import { CreateReferralRewardDTO } from "./update/CreateReferralRewardDTO";
 import { CreateReferralRewardUseCase } from "./update/CreateReferralRewardUseCase";
 
 @controller("/referral")
 export class ReferralRewardController implements interfaces.Controller {
-  constructor(private createReferralRewardUseCase: CreateReferralRewardUseCase) {}
+  constructor(
+    private createReferralRewardUseCase: CreateReferralRewardUseCase,
+    private inMemoryHashTable: InMemoryHashTable
+  ) {}
 
   @httpPost("/")
-  private async addReferralBonus(@requestBody() body: CreateReferralRewardDTO, @response() response): Promise<INPC> {
+  private async addReferralBonus(
+    @requestBody() body: CreateReferralRewardDTO,
+    @response() response,
+    @request() request
+  ): Promise<INPC> {
     try {
       const characterId = body.characterId;
 
       if (!characterId) {
         throw new BadRequestError("CharacterId field is required.");
       }
+
+      const ip = request.ip || request.headers["x-forwarded-for"] || request.connection.remoteAddress;
+
+      const referralBonusIPs = Boolean(await this.inMemoryHashTable.get("referral-bonus-ips", ip));
+
+      if (referralBonusIPs) {
+        return response.status(HttpStatus.OK).json({ message: "Referral bonus already added, skipping." });
+      }
+
+      await this.inMemoryHashTable.set("referral-bonus-ips", ip, true);
 
       await this.createReferralRewardUseCase.awardReferralBonusToCharacter(characterId, 1);
 
