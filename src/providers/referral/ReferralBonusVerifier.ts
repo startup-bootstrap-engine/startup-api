b7feb260467d438cba152c22f52cfb326a25b6a2
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-01-07 14:04:12 -0800

    Refactor CreateReferralRewardUseCase class

---
+++ src/providers/referral/ReferralBonusVerifier.ts
@@ -0,0 +1,106 @@
+import { InMemoryHashTable } from "@providers/database/InMemoryHashTable";
+import { RSAEncryption } from "@providers/encryption/RSAEncryption";
+import { Request } from "express";
+import { provide } from "inversify-binding-decorators";
+import requestIp from "request-ip";
+
+@provide(ReferralBonusVerifier)
+export class ReferralBonusVerifier {
+  constructor(private inMemoryHashTable: InMemoryHashTable, private rsaEncryption: RSAEncryption) {}
+
+  public async isReferralBonusAlreadyAdded(request: Request, deviceFingerprint: string): Promise<boolean> {
+    try {
+      const wasRequestIPAlreadyUsed = await this.wasRequestIPAlreadyUsed(request);
+
+      if (wasRequestIPAlreadyUsed) {
+        return true;
+      }
+
+      const wasDeviceFingerprintAlreadyUsed = await this.wasDeviceFingerprintAlreadyUsed(deviceFingerprint);
+
+      if (wasDeviceFingerprintAlreadyUsed) {
+        return true;
+      }
+
+      return false;
+    } catch (error) {
+      console.error(error);
+      return true;
+    }
+  }
+
+  private async wasDeviceFingerprintAlreadyUsed(deviceFingerprint: string): Promise<boolean> {
+    if (!deviceFingerprint) {
+      throw new Error("Invalid or missing device fingerprint");
+    }
+
+    console.log(`Encrypted Device fingerprint: ${deviceFingerprint}`);
+
+    const decryptedDeviceFingerprint = this.rsaEncryption.decryptWithPrivateKey(deviceFingerprint);
+
+    if (!decryptedDeviceFingerprint) {
+      throw new Error("Failed to decrypt device fingerprint");
+    }
+
+    console.log(`Decrypted Device fingerprint: ${decryptedDeviceFingerprint}`);
+
+    deviceFingerprint = decryptedDeviceFingerprint;
+
+    if (!this.isValidFingerprint(Number(deviceFingerprint))) {
+      throw new Error(`Invalid device fingerprint pattern ${deviceFingerprint}.`);
+    }
+
+    const isDeviceFingerprintAdded = Boolean(
+      await this.inMemoryHashTable.get("referral-bonus-device-fingerprint", deviceFingerprint)
+    );
+
+    if (isDeviceFingerprintAdded) {
+      console.log(`Device fingerprint ${deviceFingerprint} already exists`);
+      return true;
+    }
+
+    console.log(`Device fingerprint: ${deviceFingerprint} does not exist.`);
+    await this.inMemoryHashTable.set("referral-bonus-device-fingerprint", deviceFingerprint, true);
+
+    return false;
+  }
+
+  private async wasRequestIPAlreadyUsed(request: Request): Promise<boolean> {
+    try {
+      const clientIp = requestIp.getClientIp(request);
+
+      if (!clientIp) {
+        throw new Error("Invalid or missing client IP");
+      }
+
+      const isClientIpAdded = Boolean(await this.inMemoryHashTable.get("referral-bonus-ip-list", clientIp));
+
+      if (isClientIpAdded) {
+        console.log(`Client IP ${clientIp} already exists`);
+        return true;
+      }
+
+      console.log(`Client IP ${clientIp} does not exist. Proceeding...`);
+
+      await this.inMemoryHashTable.set("referral-bonus-ip", clientIp, true);
+
+      return false;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  private isValidFingerprint(n: number): boolean {
+    // Define the limits for 32-bit unsigned integer
+    const MIN: number = 0; // Minimum 32-bit unsigned integer
+    const MAX: number = 4294967295; // Maximum 32-bit unsigned integer
+
+    // Check if the number is an integer and within the 32-bit unsigned integer range
+    if (Number.isInteger(n) && n >= MIN && n <= MAX) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
--- src/useCases/ModuleReferral/update/CreateReferralRewardUseCase.ts
+++ src/useCases/ModuleReferral/update/CreateReferralRewardUseCase.ts
@@ -1,158 +1,19 @@
-import { Character, ICharacter } from "@entities/ModuleCharacter/CharacterModel";
-import { ItemContainer } from "@entities/ModuleInventory/ItemContainerModel";
-import { IItem, Item } from "@entities/ModuleInventory/ItemModel";
-import { BlueprintManager } from "@providers/blueprint/BlueprintManager";
-import { CharacterInventory } from "@providers/character/CharacterInventory";
-import { CharacterItemContainer } from "@providers/character/characterItems/CharacterItemContainer";
-import { InMemoryHashTable } from "@providers/database/InMemoryHashTable";
-import { RSAEncryption } from "@providers/encryption/RSAEncryption";
-import { CraftingResourcesBlueprint } from "@providers/item/data/types/itemsBlueprintTypes";
+import { ReferralBonusAwarder } from "@providers/referral/ReferralBonusAwarder";
+import { ReferralBonusVerifier } from "@providers/referral/ReferralBonusVerifier";
 import { Request } from "express";
 import { provide } from "inversify-binding-decorators";
-import requestIp from "request-ip";
 @provide(CreateReferralRewardUseCase)
 export class CreateReferralRewardUseCase {
   constructor(
-    private characterItemContainer: CharacterItemContainer,
-    private characterInventory: CharacterInventory,
-    private blueprintManager: BlueprintManager,
-    private inMemoryHashTable: InMemoryHashTable,
-    private rsaEncryption: RSAEncryption
+    private referralBonusAwarder: ReferralBonusAwarder,
+    private referralBonusVerifier: ReferralBonusVerifier
   ) {}
 
-  public async awardReferralBonusToCharacter(characterId: string, amount: number): Promise<void> {
-    try {
-      const character = (await Character.findById(characterId).lean()) as ICharacter;
-
-      if (!character) {
-        throw new Error("Character not found");
-      }
-
-      const inventory = await this.characterInventory.getInventory(character);
-
-      if (!inventory) {
-        throw new Error("Inventory not found");
-      }
-
-      const inventoryContainer = await ItemContainer.findById(inventory.itemContainer).lean();
-
-      if (!inventoryContainer) {
-        throw new Error("Inventory container not found");
-      }
-
-      const socialCrystalItemBlueprint = (await this.blueprintManager.getBlueprint(
-        "items",
-        CraftingResourcesBlueprint.SocialCrystal
-      )) as Partial<IItem>;
-
-      const newItem = new Item({
-        ...socialCrystalItemBlueprint,
-        stackQty: amount,
-      });
-
-      await newItem.save();
-
-      await this.characterItemContainer.addItemToContainer(newItem, character, inventoryContainer._id);
-    } catch (error) {
-      console.error(error);
-
-      throw error;
-    }
+  public awardReferralBonusToCharacter(characterId: string, amount: number): Promise<void> {
+    return this.referralBonusAwarder.awardReferralBonusToCharacter(characterId, amount);
   }
 
   public async isReferralBonusAlreadyAdded(request: Request, deviceFingerprint: string): Promise<boolean> {
-    try {
-      const wasRequestIPAlreadyUsed = await this.wasRequestIPAlreadyUsed(request);
-
-      if (wasRequestIPAlreadyUsed) {
-        return true;
-      }
-
-      const wasDeviceFingerprintAlreadyUsed = await this.wasDeviceFingerprintAlreadyUsed(deviceFingerprint);
-
-      if (wasDeviceFingerprintAlreadyUsed) {
-        return true;
-      }
-
-      return false;
-    } catch (error) {
-      console.error(error);
-      return true;
-    }
-  }
-
-  private async wasDeviceFingerprintAlreadyUsed(deviceFingerprint: string): Promise<boolean> {
-    if (!deviceFingerprint) {
-      throw new Error("Invalid or missing device fingerprint");
-    }
-
-    console.log(`Encrypted Device fingerprint: ${deviceFingerprint}`);
-
-    const decryptedDeviceFingerprint = this.rsaEncryption.decryptWithPrivateKey(deviceFingerprint);
-
-    if (!decryptedDeviceFingerprint) {
-      throw new Error("Failed to decrypt device fingerprint");
-    }
-
-    console.log(`Decrypted Device fingerprint: ${decryptedDeviceFingerprint}`);
-
-    deviceFingerprint = decryptedDeviceFingerprint;
-
-    if (!this.isValidFingerprint(Number(deviceFingerprint))) {
-      throw new Error(`Invalid device fingerprint pattern ${deviceFingerprint}.`);
-    }
-
-    const isDeviceFingerprintAdded = Boolean(
-      await this.inMemoryHashTable.get("referral-bonus-device-fingerprint", deviceFingerprint)
-    );
-
-    if (isDeviceFingerprintAdded) {
-      console.log(`Device fingerprint ${deviceFingerprint} already exists`);
-      return true;
-    }
-
-    console.log(`Device fingerprint: ${deviceFingerprint} does not exist.`);
-    await this.inMemoryHashTable.set("referral-bonus-device-fingerprint", deviceFingerprint, true);
-
-    return false;
-  }
-
-  private async wasRequestIPAlreadyUsed(request: Request): Promise<boolean> {
-    try {
-      const clientIp = requestIp.getClientIp(request);
-
-      if (!clientIp) {
-        throw new Error("Invalid or missing client IP");
-      }
-
-      const isClientIpAdded = Boolean(await this.inMemoryHashTable.get("referral-bonus-ip-list", clientIp));
-
-      if (isClientIpAdded) {
-        console.log(`Client IP ${clientIp} already exists`);
-        return true;
-      }
-
-      console.log(`Client IP ${clientIp} does not exist. Proceeding...`);
-
-      await this.inMemoryHashTable.set("referral-bonus-ip", clientIp, true);
-
-      return false;
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  }
-
-  private isValidFingerprint(n: number): boolean {
-    // Define the limits for 32-bit unsigned integer
-    const MIN: number = 0; // Minimum 32-bit unsigned integer
-    const MAX: number = 4294967295; // Maximum 32-bit unsigned integer
-
-    // Check if the number is an integer and within the 32-bit unsigned integer range
-    if (Number.isInteger(n) && n >= MIN && n <= MAX) {
-      return true;
-    } else {
-      return false;
-    }
+    return await this.referralBonusVerifier.isReferralBonusAlreadyAdded(request, deviceFingerprint);
   }
 }
