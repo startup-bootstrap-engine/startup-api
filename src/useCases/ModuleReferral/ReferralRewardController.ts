76f04d2af63aedb3e110fe153a2da7130fbeb1af
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-01-07 12:54:31 -0800

    Add rate limiting and request IP tracking

--- src/useCases/ModuleReferral/ReferralRewardController.ts
+++ src/useCases/ModuleReferral/ReferralRewardController.ts
@@ -1,19 +1,22 @@
 import { INPC } from "@entities/ModuleNPC/NPCModel";
-import { InMemoryHashTable } from "@providers/database/InMemoryHashTable";
 import { BadRequestError } from "@providers/errors/BadRequestError";
 import { HttpStatus } from "@rpg-engine/shared";
+import rateLimit from "express-rate-limit";
 import { controller, httpPost, interfaces, request, requestBody, response } from "inversify-express-utils";
 import { CreateReferralRewardDTO } from "./update/CreateReferralRewardDTO";
 import { CreateReferralRewardUseCase } from "./update/CreateReferralRewardUseCase";
 
+const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
+
 @controller("/referral")
 export class ReferralRewardController implements interfaces.Controller {
-  constructor(
-    private createReferralRewardUseCase: CreateReferralRewardUseCase,
-    private inMemoryHashTable: InMemoryHashTable
-  ) {}
+  constructor(private createReferralRewardUseCase: CreateReferralRewardUseCase) {}
 
-  @httpPost("/")
+  @httpPost("/", rateLimiter)
   private async addReferralBonus(
     @requestBody() body: CreateReferralRewardDTO,
     @response() response,
@@ -28,7 +31,7 @@ export class ReferralRewardController implements interfaces.Controller {
 
       const isReferralBonusAlreadyAdded = await this.createReferralRewardUseCase.isReferralBonusAlreadyAdded(
         request,
-        deviceFingerprint!
+        String(deviceFingerprint)!
       );
 
       if (isReferralBonusAlreadyAdded) {
--- src/useCases/ModuleReferral/update/CreateReferralRewardUseCase.ts
+++ src/useCases/ModuleReferral/update/CreateReferralRewardUseCase.ts
@@ -8,7 +8,7 @@ import { InMemoryHashTable } from "@providers/database/InMemoryHashTable";
 import { CraftingResourcesBlueprint } from "@providers/item/data/types/itemsBlueprintTypes";
 import { Request } from "express";
 import { provide } from "inversify-binding-decorators";
-
+import requestIp from "request-ip";
 @provide(CreateReferralRewardUseCase)
 export class CreateReferralRewardUseCase {
   constructor(
@@ -59,30 +59,86 @@ export class CreateReferralRewardUseCase {
   }
 
   public async isReferralBonusAlreadyAdded(request: Request, deviceFingerprint: string): Promise<boolean> {
-    if (deviceFingerprint) {
-      const isDeviceFingerprintAdded = Boolean(
-        await this.inMemoryHashTable.get("referral-bonus-device-fingerprint", deviceFingerprint)
-      );
+    try {
+      const wasRequestIPAlreadyUsed = await this.wasRequestIPAlreadyUsed(request);
 
-      if (isDeviceFingerprintAdded) {
+      if (wasRequestIPAlreadyUsed) {
         return true;
       }
 
-      await this.inMemoryHashTable.set("referral-bonus-device-fingerprint", deviceFingerprint, true);
-    }
+      const wasDeviceFingerprintAlreadyUsed = await this.wasDeviceFingerprintAlreadyUsed(deviceFingerprint);
 
-    const ip = String(request.ip || request.headers["x-forwarded-for"] || request.connection.remoteAddress);
-
-    if (ip) {
-      const referralBonusIPs = Boolean(await this.inMemoryHashTable.get("referral-bonus-ips", ip));
-
-      if (referralBonusIPs) {
+      if (wasDeviceFingerprintAlreadyUsed) {
         return true;
       }
 
-      await this.inMemoryHashTable.set("referral-bonus-ips", ip, true);
+      return false;
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
+  }
+
+  private async wasDeviceFingerprintAlreadyUsed(deviceFingerprint: string): Promise<boolean> {
+    if (!deviceFingerprint) {
+      throw new Error("Invalid or missing device fingerprint");
+    }
+
+    if (!this.isValidFingerprint(Number(deviceFingerprint))) {
+      throw new Error(`Invalid device fingerprint ${deviceFingerprint}.`);
+    }
+
+    const isDeviceFingerprintAdded = Boolean(
+      await this.inMemoryHashTable.get("referral-bonus-device-fingerprint", deviceFingerprint)
+    );
+
+    if (isDeviceFingerprintAdded) {
+      console.log(`Device fingerprint ${deviceFingerprint} already exists`);
+      return true;
+    }
+
+    console.log(`Device fingerprint: ${deviceFingerprint} does not exist.`);
+    await this.inMemoryHashTable.set("referral-bonus-device-fingerprint", deviceFingerprint, true);
 
     return false;
+  }
+
+  private async wasRequestIPAlreadyUsed(request: Request): Promise<boolean> {
+    try {
+      const clientIp = requestIp.getClientIp(request);
+
+      if (!clientIp) {
+        throw new Error("Invalid or missing client IP");
+      }
+
+      const isClientIpAdded = Boolean(await this.inMemoryHashTable.get("referral-bonus-ip", clientIp));
+
+      if (isClientIpAdded) {
+        console.log(`Client IP ${clientIp} already exists`);
+        return true;
+      }
+
+      console.log(`Client IP ${clientIp} does not exist. Adding referral bonus`);
+
+      await this.inMemoryHashTable.set("referral-bonus-ip", clientIp, true);
+
+      return false;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  private isValidFingerprint(n: number): boolean {
+    // Define the limits for 32-bit unsigned integer
+    const MIN: number = 0; // Minimum 32-bit unsigned integer
+    const MAX: number = 4294967295; // Maximum 32-bit unsigned integer
+
+    // Check if the number is an integer and within the 32-bit unsigned integer range
+    if (Number.isInteger(n) && n >= MIN && n <= MAX) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
