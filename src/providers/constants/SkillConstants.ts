d7414857131a6ffc8204bfa1407998b63985354c
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-01-30 17:44:28 -0800

    increased XP ratio a bit

--- src/providers/constants/SkillConstants.ts
+++ src/providers/constants/SkillConstants.ts
@@ -4,7 +4,7 @@ import { CharacterClass, LifeBringerRaces, Modes, ShadowWalkerRaces } from "@rpg
 export const DAMAGE_ATTRIBUTE_WEIGHT = 1.15;
 export const DAMAGE_COMBAT_SKILL_WEIGHT = 1.6;
 
-export const EXP_RATIO = 1.5;
+export const EXP_RATIO = 1.6;
 
 export const SP_INCREASE_BASE = 0.8;
 
