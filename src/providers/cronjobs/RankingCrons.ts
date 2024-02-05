cc5432695cc39d85c3c40aa3dcdc991b397dd4c4
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-02-04 17:23:22 -0800

    discord ranking disable

--- src/providers/cronjobs/RankingCrons.ts
+++ src/providers/cronjobs/RankingCrons.ts
@@ -1,8 +1,6 @@
 import { DiscordBot } from "@providers/discord/DiscordBot";
 import { RankingGetInfo } from "@providers/ranking/RankingGetInfo";
 
-import { Colors } from "discord.js";
-
 import { RankingCache } from "@providers/ranking/RankingCache";
 import { provide } from "inversify-binding-decorators";
 import { CronJobScheduler } from "./CronJobScheduler";
@@ -27,52 +25,51 @@ export class RankingCrons {
   }
 
   private async topLevelGlobal(): Promise<void> {
-    const result = await this.rankingGetInfo.topLevelGlobal();
+    await this.rankingGetInfo.topLevelGlobal();
 
-    let message = "";
-    let index = 1;
+    // let message = "";
+    // let index = 1;
 
-    result.forEach((char) => {
-      if (index <= 10) {
-        message += `${index}- Name: [${char.name}] - Level: [${Math.round(char.level)}] \n`;
-        index++;
-      }
-    });
+    // result.forEach((char) => {
+    //   if (index <= 10) {
+    //     message += `${index}- Name: [${char.name}] - Level: [${Math.round(char.level)}] \n`;
+    //     index++;
+    //   }
+    // });
 
-    const title = "Top 10 Global Level!";
+    // const title = "Top 10 Global Level!";
 
-    await this.discordBot.sendMessageWithColor(message, "rankings", title, Colors.Purple);
+    // await this.discordBot.sendMessageWithColor(message, "rankings", title, Colors.Purple);
   }
 
   private async topLevelClass(): Promise<void> {
-    const result = this.rankingGetInfo.topLevelClass();
+    await this.rankingGetInfo.topLevelClass();
 
-    for (const rank of Object.values(result)) {
-      let message = "";
-      const title = `Top 10 ${rank.class}!`;
+    // for (const rank of Object.values(result)) {
+    //   let message = "";
+    //   const title = `Top 10 ${rank.class}!`;
 
-      rank.topPlayers.forEach((char, index) => {
-        message += `${index + 1}- Name: [${char.name}] - Level: [${Math.round(char.level)}]\n`;
-      });
+    //   rank.topPlayers.forEach((char, index) => {
+    //     message += `${index + 1}- Name: [${char.name}] - Level: [${Math.round(char.level)}]\n`;
+    //   });
 
-      await this.discordBot.sendMessageWithColor(message, "rankings", title, Colors.Gold);
-    }
+    //   await this.discordBot.sendMessageWithColor(message, "rankings", title, Colors.Gold);
+    // }
   }
 
   private async topLevelBySkillType(): Promise<void> {
-    const top10ForAllSkills = await this.rankingGetInfo.topLevelBySkillType();
+    await this.rankingGetInfo.topLevelBySkillType();
 
-    for (const ranking of top10ForAllSkills) {
-      let message = "";
+    // for (const ranking of top10ForAllSkills) {
+    //   let message = "";
 
-      ranking.top10.sort((a, b) => b.level - a.level);
+    //   ranking.top10.sort((a, b) => b.level - a.level);
 
-      const title = `Top 10 ${ranking.skill.charAt(0).toUpperCase() + ranking.skill.slice(1)} Skill!`;
-      ranking.top10.forEach((char, index) => {
-        message += `${index + 1}- Name: [${char.name}] - Level: [${char.level}]\n`;
-      });
+    //   const title = `Top 10 ${ranking.skill.charAt(0).toUpperCase() + ranking.skill.slice(1)} Skill!`;
+    //   ranking.top10.forEach((char, index) => {
+    //     message += `${index + 1}- Name: [${char.name}] - Level: [${char.level}]\n`;
+    //   });
 
-      await this.discordBot.sendMessageWithColor(message, "rankings", title, Colors.Aqua);
-    }
+    //   await this.discordBot.sendMessageWithColor(message, "rankings", title, Colors.Aqua);
   }
 }
