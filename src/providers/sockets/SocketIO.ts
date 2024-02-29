245eaafb4f2b9b269245b2e7df251e70895b3eb9
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-02-28 23:22:34 -0800

    pubsub redis error long

--- src/providers/sockets/SocketIO.ts
+++ src/providers/sockets/SocketIO.ts
@@ -38,11 +38,11 @@ export class SocketIO implements ISocket {
         this.socket.adapter(createAdapter(pubClient, subClient));
 
         pubClient.on("error", (err) => {
-          console.log(err.message);
+          console.log("pubClient: ", err.message);
         });
 
         subClient.on("error", (err) => {
-          console.log(err.message);
+          console.log("subClient: ", err.message);
         });
 
         await Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
