0584396935127217082c3b667885201cb0a78683
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-03-12 20:13:54 -0700

    marketplace adjustments

--- src/providers/constants/MarketplaceConstants.ts
+++ src/providers/constants/MarketplaceConstants.ts
@@ -2,4 +2,4 @@ import { OthersBlueprint } from "@providers/item/data/types/itemsBlueprintTypes"
 
 export const BLOCKED_ITEMS_KEY_FOR_MARKETPLACE: string[] = [OthersBlueprint.GoldCoin];
 
-export const MARKETPLACE_ITEM_MAX_WEEKS_LENGTH = 3; // max weeks an item can be on the marketplace
+export const MARKETPLACE_ITEM_MAX_WEEKS_LENGTH = 1; // max weeks an item can be on the marketplace
