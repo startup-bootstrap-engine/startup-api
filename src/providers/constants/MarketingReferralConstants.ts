79c5ed554712a49548533bda3f004b0efd309613
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-03-23 19:38:24 -0700

    Update social crystal crafting constants

--- src/providers/constants/MarketingReferralConstants.ts
+++ src/providers/constants/MarketingReferralConstants.ts
@@ -1,10 +1,10 @@
 // Marketing
 
-export const CRAFTING_SOCIAL_CRYSTAL_REQUIREMENT_RATIO = 1.75; // higher than 1 means more social crystals required to craft items
+export const CRAFTING_SOCIAL_CRYSTAL_REQUIREMENT_RATIO = 1.85; // higher than 1 means more social crystals required to craft items
 
 export const CRAFTING_SOCIAL_CRYSTAL_MIN_TIER_REQUIREMENT = 8; // minimum tier to require social crystals to craft items
 
-export const CRAFTING_SOCIAL_CRYSTAL_MAX_CAP_REQUIREMENT = 8; // max cap of social crystals required to craft items
+export const CRAFTING_SOCIAL_CRYSTAL_MAX_CAP_REQUIREMENT = 9; // max cap of social crystals required to craft items
 
 export const CRAFTING_SOCIAL_CRYSTAL_DAILY_REWARD_CHANCE = 25; // chance to get social crystals as daily reward, in %
 
