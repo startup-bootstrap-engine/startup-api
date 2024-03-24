962d49a84ec5a95750c72130dfac58f355e5beec
Achiles Gomes <achillesgomess@gmail.com>
2024-03-24 20:45:03 -0300

    fix celestial defender recipe

--- src/providers/useWith/recipes/swords/tier18/recipeCelestialDefender.ts
+++ src/providers/useWith/recipes/swords/tier18/recipeCelestialDefender.ts
@@ -7,7 +7,7 @@ export const recipeCelestialDefender: IUseWithCraftingRecipe = {
   outputQtyRange: [1, 1],
   requiredItems: [
     {
-      key: SwordsBlueprint.CelestialDefender,
+      key: SwordsBlueprint.FrostfireLongblade,
       qty: 1,
     },
     {
