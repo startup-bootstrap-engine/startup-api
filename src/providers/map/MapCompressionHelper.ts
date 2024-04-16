fef594550e1ff10be295b1f6b75b8441f7ca8ec8
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-04-16 10:06:20 -0700

    zip fix

--- src/providers/map/MapCompressionHelper.ts
+++ src/providers/map/MapCompressionHelper.ts
@@ -3,11 +3,18 @@ import fs from "fs";
 import JSZip from "jszip";
 
 export const createZipMap = async (mapName: string, mapObject: object, pathToSave: string): Promise<void> => {
+  const filePath = `${pathToSave}/${mapName}.zip`;
+
+  // Delete the file if it exists
+  if (fs.existsSync(filePath)) {
+    await fs.promises.unlink(filePath);
+  }
+
   const data = compress(mapObject);
   const zip = new JSZip();
   zip.file(`${mapName}.txt`, JSON.stringify(data), {
     unixPermissions: "755",
   });
   const buffer = await zip.generateAsync({ type: "uint8array" });
-  await fs.createWriteStream(`${pathToSave}/${mapName}.zip`).write(buffer);
+  await fs.promises.writeFile(filePath, buffer);
 };
