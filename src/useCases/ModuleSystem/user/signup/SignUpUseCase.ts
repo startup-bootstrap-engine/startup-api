5cc15779f19c65321d9d0e55db89ddea4a6efbf8
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-04-15 22:26:45 -0700

    Refactor SignUpUseCase to remove unused code and comments

--- src/useCases/ModuleSystem/user/signup/SignUpUseCase.ts
+++ src/useCases/ModuleSystem/user/signup/SignUpUseCase.ts
@@ -5,8 +5,6 @@ import { TS } from "@providers/translation/TranslationHelper";
 import { UserRepository } from "@repositories/ModuleSystem/user/UserRepository";
 import { provide } from "inversify-binding-decorators";
 
-import { TransactionalEmail } from "../../../../../emails/TransactionalEmail";
-import { appEnv } from "../../../../providers/config/env";
 import { ConflictError } from "../../../../providers/errors/ConflictError";
 import { AuthSignUpDTO } from "../AuthDTO";
 
@@ -33,19 +31,19 @@ export class SignUpUseCase {
 
     const newUser = await this.userRepository.signUp(authSignUpDTO);
 
-    if (newUser) {
-      console.log("🤖 Submitting new user's welcome e-mail");
+    // if (newUser) {
+    //   console.log("🤖 Submitting new user's welcome e-mail");
 
-      await TransactionalEmail.send(newUser.email, TS.translate("email", "welcome"), "welcome", {
-        newAccountEmailTitle: TS.translate("email", "welcome"),
-        newAccountEmailFirstParagraph: TS.translate("email", "newAccountEmailFirstParagraph", {
-          appName: appEnv.general.APP_NAME!,
-        }),
-        newAccountEmailForReference: TS.translate("email", "newAccountEmailForReference"),
-        userEmail: newUser.email,
-        newAccountEmailBottom: TS.translate("email", "newAccountEmailBottom", { appName: appEnv.general.APP_NAME! }),
-      });
-    }
+    //   await TransactionalEmail.send(newUser.email, TS.translate("email", "welcome"), "welcome", {
+    //     newAccountEmailTitle: TS.translate("email", "welcome"),
+    //     newAccountEmailFirstParagraph: TS.translate("email", "newAccountEmailFirstParagraph", {
+    //       appName: appEnv.general.APP_NAME!,
+    //     }),
+    //     newAccountEmailForReference: TS.translate("email", "newAccountEmailForReference"),
+    //     userEmail: newUser.email,
+    //     newAccountEmailBottom: TS.translate("email", "newAccountEmailBottom", { appName: appEnv.general.APP_NAME! }),
+    //   });
+    // }
 
     return newUser;
   }
