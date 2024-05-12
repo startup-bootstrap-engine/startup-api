8c8b518aae2e63cc91e7b4c8a766d8fde7847e43
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-05-12 10:12:41 -0700

    fix

--- src/providers/party/PartyClasses.ts
+++ src/providers/party/PartyClasses.ts
@@ -1,10 +1,11 @@
+import { CharacterClass } from "@rpg-engine/shared";
 import { provide } from "inversify-binding-decorators";
 import { ICharacterParty } from "./PartyTypes";
 
 @provide(PartyClasses)
 export class PartyClasses {
   public getDifferentClasses(party: ICharacterParty): number {
-    const leaderClass = party.leader.class;
+    const leaderClass = party.leader.class as CharacterClass;
     const memberClasses = party.members.map((member) => member.class);
 
     memberClasses.push(leaderClass);
