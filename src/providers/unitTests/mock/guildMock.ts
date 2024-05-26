f39293f6cd1521033b212033983babdba1e57ae1
anuradha fernando <anufdo@gmail.com>
2024-05-26 17:49:47 +0530

    RPG-3460-RPG-3505-API-Guild-Schema

---
+++ src/providers/unitTests/mock/guildMock.ts
@@ -0,0 +1,18 @@
+import { IGuild } from "@entities/ModuleSystem/GuildModel";
+import { Types } from "mongoose";
+
+export const guildMock: Partial<IGuild> = {
+  name: "Example Guild",
+  tag: "ExpGuild",
+  coatOfArms: "example.png",
+  guildLeader: "6233ff328f3b09002fe32f9b" as unknown as Types.ObjectId,
+  members: ["6233ff328f3b09002fe32f9c"],
+  territoriesOwned: [
+    {
+      map: "example",
+      lootShare: 10,
+      controlPoint: true,
+    },
+  ],
+  guildSkills: undefined,
+};
