d231dbdb8d0e78a3044f7c078f45ad770d294149
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-05-27 21:20:21 -0700

    blocking maps

--- src/providers/constants/MapConstants.ts
+++ src/providers/constants/MapConstants.ts
@@ -14,3 +14,5 @@ export const NO_LIGHTENING_MAPS = ["ilya-village-interiors", "arena-hell"];
 export const UNIT_TESTING_MAPS = ["unit-test-map.json", "example.json", "unit-test-map-negative-coordinate.json"];
 
 export const DYNAMIC_MAP_DARKNESS = 0.8; // TODO: Set to 0.95 when there're torches or light spell
+
+export const TEMPORARILY_BLOCKED_MAPS = ["elf-continent_2"];
