567a06c38eacbb776805b23dbad0c0f39cba2190
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-05-31 15:36:07 -0700

    fix

--- src/providers/useWith/recipes/daggers/index.ts
+++ src/providers/useWith/recipes/daggers/index.ts
@@ -1,16 +1,10 @@
 import { DaggersBlueprint } from "@providers/item/data/types/itemsBlueprintTypes";
 import { IUseWithCraftingRecipe } from "@providers/useWith/useWithTypes";
 import { recipeFrostbiteBlade } from "../swords/tier3/recipeFrostbiteBlade";
-import { recipeAstralDagger } from "./tier11/recipeAstralDagger";
-import { recipeAzureDagger } from "./tier4/recipeAzureDagger";
-import { recipeFrostDagger } from "./tier2/recipeFrostDagger";
-import { recipeGoldenDagger } from "./tier5/recipeGoldenDagger";
-import { recipeHexBladeDagger } from "./tier8/recipeHexBladeDagger";
-import { recipeMistfireDagger } from "./tier9/recipeMistfireDagger";
-import { recipeSpiritBlade } from "./tier9/recipeSpiritBlade";
-import { recipeStarshardDagger } from "./tier11/recipeStarshardDagger";
-import { recipeStormswiftDagger } from "./tier10/recipeStormswiftDagger";
 import { recipeWoodenDagger } from "./tier0/recipeWoodenDagger";
+import { recipeStormswiftDagger } from "./tier10/recipeStormswiftDagger";
+import { recipeAstralDagger } from "./tier11/recipeAstralDagger";
+import { recipeStarshardDagger } from "./tier11/recipeStarshardDagger";
 import { recipeAzurefireDagger } from "./tier12/recipeAzurefireDagger";
 import { recipeEmberbladeDagger } from "./tier12/recipeEmberbladeDagger";
 import { recipeShadowstrikeDagger } from "./tier12/recipeShadowstrikeDagger";
@@ -21,11 +15,16 @@ import { recipeBloodmoonDagger } from "./tier14/recipeBloodmoonDagger";
 import { recipeMoonlightDagger } from "./tier14/recipeMoonlightDagger";
 import { recipeThunderstrikeDagger } from "./tier15/recipeThunderstrikeDagger";
 import { recipeVenomousFangDagger } from "./tier15/recipeVenomousFangDagger";
-import { recipeCrimsonLotusDagger } from "./tier16/recipeCrimsonLotusDagger";
 import { recipeEtherealVeilDagger } from "./tier16/recipeEtherealVeilDagger";
 import { recipeSeraphicDagger } from "./tier17/recipeSeraphicDagger";
 import { recipeTwilightThornDagger } from "./tier17/recipeTwilightThornDagger";
 import { recipeObsidianEdgeDagger } from "./tier18/recipeObsidianEdgeDagger";
+import { recipeFrostDagger } from "./tier2/recipeFrostDagger";
+import { recipeAzureDagger } from "./tier4/recipeAzureDagger";
+import { recipeGoldenDagger } from "./tier5/recipeGoldenDagger";
+import { recipeHexBladeDagger } from "./tier8/recipeHexBladeDagger";
+import { recipeMistfireDagger } from "./tier9/recipeMistfireDagger";
+import { recipeSpiritBlade } from "./tier9/recipeSpiritBlade";
 
 export const recipeDaggersIndex: Record<string, IUseWithCraftingRecipe[]> = {
   [DaggersBlueprint.FrostDagger]: [recipeFrostDagger],
@@ -44,7 +43,6 @@ export const recipeDaggersIndex: Record<string, IUseWithCraftingRecipe[]> = {
   [DaggersBlueprint.CelestialShardDagger]: [recipeCelestialShardDagger],
   [DaggersBlueprint.MoonlightDagger]: [recipeMoonlightDagger],
   [DaggersBlueprint.VenomousFangDagger]: [recipeVenomousFangDagger],
-  [DaggersBlueprint.CrimsonLotusDagger]: [recipeCrimsonLotusDagger],
   [DaggersBlueprint.SeraphicDagger]: [recipeSeraphicDagger],
   [DaggersBlueprint.ObsidianEdgeDagger]: [recipeObsidianEdgeDagger],
   [DaggersBlueprint.SolarflareDagger]: [recipeSolarflareDagger],
