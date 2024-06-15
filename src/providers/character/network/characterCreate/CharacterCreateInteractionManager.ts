c6f759206f0a3576d415f643b56cd27450732615
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-06-15 14:28:36 -0700

    chore: Refactor CharacterCreateInteractionManager to remove unnecessary async/await

--- src/providers/character/network/characterCreate/CharacterCreateInteractionManager.ts
+++ src/providers/character/network/characterCreate/CharacterCreateInteractionManager.ts
@@ -69,8 +69,8 @@ export class CharacterCreateInteractionManager {
     };
   }
 
-  public async startNPCInteractions(character: ICharacter): Promise<void> {
-    await this.npcManager.startNearbyNPCsBehaviorLoop(character);
+  public startNPCInteractions(character: ICharacter): void {
+    void this.npcManager.startNearbyNPCsBehaviorLoop(character);
     void this.npcWarn.warnCharacterAboutNPCsInView(character, { always: true });
     void this.itemView.warnCharacterAboutItemsInView(character, { always: true }); // Don't await to avoid blocking
   }
