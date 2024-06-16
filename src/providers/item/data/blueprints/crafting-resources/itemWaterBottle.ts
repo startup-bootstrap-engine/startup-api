10bc45aa11201495c6c81a56a0478bf1ab9291d7
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-06-15 22:38:58 -0700

    refactor: Update itemWaterBottle to use ClearBurningUsableEffect

--- src/providers/item/data/blueprints/crafting-resources/itemWaterBottle.ts
+++ src/providers/item/data/blueprints/crafting-resources/itemWaterBottle.ts
@@ -1,15 +1,17 @@
 import { ICraftableItemBlueprint, ItemSubType, ItemType } from "@rpg-engine/shared";
 import { CraftingResourcesBlueprint } from "../../types/itemsBlueprintTypes";
+import { UsableEffectsBlueprint } from "../../usableEffects/types";
 
 export const itemWaterBottle: ICraftableItemBlueprint = {
   key: CraftingResourcesBlueprint.WaterBottle,
-  type: ItemType.CraftingResource,
+  type: ItemType.Consumable,
   subType: ItemSubType.CraftingResource,
   textureAtlas: "items",
   texturePath: "crafting-resources/water-bottle.png",
   name: "Water",
-  description: "A bottle of water",
+  description: "A bottle of water. It can be used to heal burning effects.",
   weight: 0.05,
   maxStackSize: 999,
   basePrice: 3,
+  usableEffectKey: UsableEffectsBlueprint.ClearBurningUsableEffect,
 };
