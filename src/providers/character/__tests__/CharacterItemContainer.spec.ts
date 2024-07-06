d02a0f63fff00acdbe67088f8cc023633ba23b20
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-07-06 12:21:51 -0700

    more tests

--- src/providers/character/__tests__/CharacterItemContainer.spec.ts
+++ src/providers/character/__tests__/CharacterItemContainer.spec.ts
@@ -63,4 +63,60 @@ describe("CharacterItemContainer.ts", () => {
 
     expect(updatedInventoryContainer.slots[0]).toBeNull();
   });
+
+  describe("CharacterItemContainer.ts - Additional Tests", () => {
+    let testCharacter: ICharacter;
+    let inventory: IItem;
+    let inventoryContainer: IItemContainer;
+    let characterItemContainer: CharacterItemContainer;
+
+    beforeEach(async () => {
+      testCharacter = await unitTestHelper.createMockCharacter(null, { hasEquipment: true, hasInventory: true });
+
+      inventory = await testCharacter.inventory;
+      inventoryContainer = (await ItemContainer.findById(inventory.itemContainer)) as unknown as IItemContainer;
+
+      characterItemContainer = container.get<CharacterItemContainer>(CharacterItemContainer);
+
+      await inMemoryHashTable.delete("container-all-items", inventoryContainer._id.toString()!);
+    });
+
+    describe("addItemToContainer", () => {
+      it("should return false if the character does not have an inventory container", async () => {
+        const testItem = await unitTestHelper.createMockItem();
+
+        const result = await characterItemContainer.addItemToContainer(testItem, testCharacter, "invalid-container-id");
+
+        expect(result).toBeFalsy();
+      });
+
+      it("should successfully remove an item from a container", async () => {
+        const testItem = await unitTestHelper.createMockItem();
+
+        await unitTestHelper.addItemsToContainer(inventoryContainer, 10, [testItem]);
+
+        expect(inventoryContainer.slots[0]._id).toEqual(testItem._id);
+
+        await characterItemContainer.removeItemFromContainer(testItem, testCharacter, inventoryContainer);
+
+        const updatedInventoryContainer = (await ItemContainer.findById(
+          inventoryContainer.id
+        )) as unknown as IItemContainer;
+
+        expect(updatedInventoryContainer.slots[0]).toBeNull();
+      });
+
+      it("should return false if trying to remove an item that does not exist in the container", async () => {
+        const nonExistentItem = await unitTestHelper.createMockItem();
+
+        const result = await characterItemContainer.removeItemFromContainer(
+          nonExistentItem,
+          testCharacter,
+          inventoryContainer
+        );
+
+        expect(result).toBeFalsy();
+      });
+    });
+  });
 });
