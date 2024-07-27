c72cbe20f91fc3ef255313b14cb2e203ce6b5b70
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-07-27 16:49:28 -0700

    hotfix

--- src/providers/npc/data/blueprints/friendly/NPCRogueDragonServant.ts
+++ src/providers/npc/data/blueprints/friendly/NPCRogueDragonServant.ts
@@ -1,10 +1,10 @@
 import { INPC } from "@entities/ModuleNPC/NPCModel";
 import { FriendlyNPCsBlueprint } from "@providers/npc/data/types/npcsBlueprintTypes";
 import { CharacterClass, CharacterGender } from "@rpg-engine/shared";
-import { generateFixedPathMovement } from "../../abstractions/BaseNeutralNPC";
+import { generateRandomMovement } from "../../abstractions/BaseNeutralNPC";
 
 export const npcRogueDragonServant = {
-  ...generateFixedPathMovement(),
+  ...generateRandomMovement(),
   name: "Rogue Dragon Servant",
   textureKey: "kobold",
   speed: 0.8,
