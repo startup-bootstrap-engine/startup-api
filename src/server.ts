262484dfdfd627955621e71f892e39e07b6f4060
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-08-24 00:11:00 -0700

    refactor: Update New Relic logging configuration

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,9 @@
+/* eslint-disable import/first */
+// Load New Relic if not running unit tests
+if (!appEnv.general.IS_UNIT_TEST) {
+  require("newrelic");
+}
+
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
 import "express-async-errors";
@@ -24,11 +30,6 @@ import { NewRelicTransactionCategory } from "@providers/types/NewRelicTypes";
 import { EnvType } from "@rpg-engine/shared/dist";
 
 dayjs.extend(duration);
-
-// Load New Relic if not running unit tests
-if (!appEnv.general.IS_UNIT_TEST) {
-  require("newrelic");
-}
 
 // Determine the port to use
 const port = getPort();
