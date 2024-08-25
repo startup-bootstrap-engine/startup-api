86e66f2dd156344844b2d835755f19e43277ea7f
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-08-25 00:52:31 -0700

    Update rpg-api and rpg-npc swarm services to restart containers

--- environment/api-refresh-containers.sh
+++ environment/api-refresh-containers.sh
@@ -16,12 +16,13 @@ npm run db:export:swarm
 echo "🐳 Restarting rpg-pathfinder swarm service..."
 docker service update --force --image definya/definya-team:rpg-pathfinder-latest --with-registry-auth --stop-grace-period=10s $PATHFINDER_SERVICE_NAME
 
+# Update the rpg-npc service to restart containers (using the same image as rpg-api)
+echo "🐳 Restarting rpg-npc swarm service..."
+docker service update --force --image definya/definya-team:api-latest --with-registry-auth --stop-grace-period=10s $NPC_SERVICE_NAME
+
 # Update the rpg-api service to restart containers
 echo "🐳 Restarting rpg-api swarm service..."
 docker service update --force --image definya/definya-team:api-latest --with-registry-auth --stop-grace-period=10s $API_SERVICE_NAME
 
-# Update the rpg-npc service to restart containers (using the same image as rpg-api)
-echo "🐳 Restarting rpg-npc swarm service..."
-docker service update --force --image definya/definya-team:api-latest --with-registry-auth --stop-grace-period=10s $NPC_SERVICE_NAME
 
 echo "🐳 Deployment complete."
