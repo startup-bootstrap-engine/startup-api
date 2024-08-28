82fa1f447fc44ed96cd5d1dcd154d1a8975caf08
İslam Uyanık <islamuyanik@gmail.com>
2024-08-28 09:38:07 +0300

    axes (#1052)

---
+++ src/providers/item/data/blueprints/axes/tier18/itemAbyssalCleave.ts
@@ -0,0 +1,72 @@
+import { EntityEffectBlueprint } from "@providers/entityEffects/data/types/entityEffectBlueprintTypes";
+import { IEquippableMeleeTier18WeaponBlueprint } from "@providers/item/data/types/TierBlueprintTypes";
+import {
+  BasicAttribute,
+  CharacterAttributes,
+  CharacterBuffDurationType,
+  CharacterBuffType,
+  EntityAttackType,
+  ItemSlotType,
+  ItemSubType,
+  ItemType,
+} from "@rpg-engine/shared";
+import { AxesBlueprint } from "../../../types/itemsBlueprintTypes";
+
+export const itemAbyssalCleave: IEquippableMeleeTier18WeaponBlueprint = {
+  key: AxesBlueprint.AbyssalCleave,
+  type: ItemType.Weapon,
+  subType: ItemSubType.Axe,
+  textureAtlas: "items",
+  texturePath: "axes/abyssal-cleave.png",
+  name: "Abyssal Cleave",
+  description: "A dark axe that rends the souls of those it strikes.",
+  weight: 4.9,
+  allowedEquipSlotType: [ItemSlotType.LeftHand, ItemSlotType.RightHand],
+  attack: 127,
+  defense: 124,
+  tier: 18,
+  rangeType: EntityAttackType.Melee,
+  basePrice: 160,
+  entityEffects: [EntityEffectBlueprint.Bleeding],
+  entityEffectChance: 94,
+  equippedBuff: [
+    {
+      type: CharacterBuffType.CharacterAttribute,
+      trait: CharacterAttributes.MaxHealth,
+      buffPercentage: 30,
+      durationType: CharacterBuffDurationType.Permanent,
+    },
+    {
+      type: CharacterBuffType.CharacterAttribute,
+      trait: CharacterAttributes.MaxMana,
+      buffPercentage: 30,
+      durationType: CharacterBuffDurationType.Permanent,
+    },
+    {
+      type: CharacterBuffType.Skill,
+      trait: BasicAttribute.Magic,
+      buffPercentage: 26,
+      durationType: CharacterBuffDurationType.Permanent,
+    },
+    {
+      type: CharacterBuffType.Skill,
+      trait: BasicAttribute.MagicResistance,
+      buffPercentage: 26,
+      durationType: CharacterBuffDurationType.Permanent,
+    },
+    {
+      type: CharacterBuffType.Skill,
+      trait: BasicAttribute.Resistance,
+      buffPercentage: 26,
+      durationType: CharacterBuffDurationType.Permanent,
+    },
+    {
+      type: CharacterBuffType.Skill,
+      trait: BasicAttribute.Strength,
+      buffPercentage: 26,
+      durationType: CharacterBuffDurationType.Permanent,
+    },
+  ],
+  equippedBuffDescription:
+    "Increases Max Health by 30%, Max Mana by 30%, Magic by 26%, Magic Resistance by 26%, Resistance by 26%, and Strength by 26%.",
+};
