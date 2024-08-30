b4cf07f3675e4aeab2b514e26fffb25e58b19300
anuradha fernando <anufdo@gmail.com>
2024-08-31 01:12:03 +0530

    RPG-3460-RPG-3691-API-Guild-steal-territory-control-from-other-guilds

--- src/providers/guild/GuildTerritoryControlPoint.ts
+++ src/providers/guild/GuildTerritoryControlPoint.ts
@@ -1,12 +1,13 @@
 import { ICharacter } from "@entities/ModuleCharacter/CharacterModel";
-import { Guild } from "@entities/ModuleSystem/GuildModel";
+import { Guild, IGuild } from "@entities/ModuleSystem/GuildModel";
 import { provide } from "inversify-binding-decorators";
+import { GuildTerritory } from "./GuildTerritory";
 
 const EXP_TO_CONTROL_POINT_RATIO = 10;
 
 @provide(GuildTerritoryControlPoint)
 export class GuildTerritoryControlPoint {
-  constructor() {}
+  constructor(private guildTerritory: GuildTerritory) {}
 
   public async updateGuildTerritoryControlPoint(map: string, character: ICharacter, exp: number): Promise<void> {
     try {
@@ -16,18 +17,49 @@ export class GuildTerritoryControlPoint {
 
       if (!guild) return;
 
+      // steal guild control points
+      const stealXp = await this.StealGuildControlPoints(map, guild as IGuild);
+      const newXp = xp + stealXp;
+
       const controlPoints = guild.controlPoints || [];
       const existingIndex = controlPoints.findIndex((cp) => cp.map === map);
 
       if (existingIndex !== -1) {
-        controlPoints[existingIndex].point += xp;
+        controlPoints[existingIndex].point += newXp;
       } else {
-        controlPoints.push({ map, point: xp });
+        controlPoints.push({ map, point: newXp });
       }
 
       await Guild.updateOne({ _id: guild._id }, { $set: { controlPoints } });
     } catch (error) {
       console.error("Error updating guild territory control point:", error);
+    }
+  }
+
+  private async StealGuildControlPoints(map: string, myGuild: IGuild): Promise<number> {
+    try {
+      const mapControlGuild = await this.guildTerritory.getGuildByTerritoryMap(map);
+      if (!mapControlGuild || mapControlGuild._id.toString() === myGuild._id.toString()) {
+        return 0;
+      }
+
+      // removing control points
+      const controlPoints = mapControlGuild.controlPoints || [];
+      const existingIndex = controlPoints.findIndex((cp) => cp.map === map);
+
+      if (existingIndex === -1 || controlPoints[existingIndex].point <= 0) {
+        return 0;
+      }
+
+      controlPoints[existingIndex].point -= 1;
+      await Guild.updateOne({ _id: mapControlGuild._id }, { $set: { controlPoints } });
+
+      // stealing control points return
+      return 1;
+    } catch (error) {
+      console.error("Error in stealing guild control points: ", error);
+
+      return 0;
     }
   }
 }
