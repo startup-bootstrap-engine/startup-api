7500c524ad3d6605057348cb7c971b2f46de7312
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-08-30 17:56:07 -0700

    refactor: Update NPC cronjob schedules for more frequent raids and shutdowns

--- src/providers/cronjobs/NPCCrons.ts
+++ src/providers/cronjobs/NPCCrons.ts
@@ -32,16 +32,14 @@ export class NPCCrons {
       await this.npcSpawnCron();
     });
 
-    this.cronJobScheduler.uniqueSchedule("npc-raid-activator", "0 * * * *", async () => {
+    this.cronJobScheduler.uniqueSchedule("npc-raid-activator", "*/10 * * * *", async () => {
       console.log("Trying to activate raids...");
       await this.npcRaidActivator.activateRaids();
     });
 
-    this.cronJobScheduler.uniqueSchedule("npc-raid-shutdown", "*/15 * * * *", async () => {
-      console.log("Trying to shutdown raids...");
+    this.cronJobScheduler.uniqueSchedule("npc-raid-shutdown", "* * * * *", async () => {
       await this.npcRaidActivator.shutdownRaids();
     });
-
     this.cronJobScheduler.uniqueSchedule("npc-freezer", "* * * * *", async () => {
       await this.npcFreezer.freezeNPCsWithoutCharactersAround();
     });
