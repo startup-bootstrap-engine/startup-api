1bba92f76431f45b141ea4798fb8082cf8d01a88
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-08-31 17:41:20 -0700

    chore: Add RabbitMQ initialization script

---
+++ environment/rabbit-init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Wait for RabbitMQ to start
+until rabbitmqctl status; do
+  echo "Waiting for RabbitMQ to start..."
+  sleep 2
+done
+
+# Create user and set permissions
+rabbitmqctl add_user $RABBITMQ_DEFAULT_USER $RABBITMQ_DEFAULT_PASS
+rabbitmqctl set_user_tags $RABBITMQ_DEFAULT_USER administrator
+rabbitmqctl set_permissions -p / $RABBITMQ_DEFAULT_USER ".*" ".*" ".*"
+
+echo "RabbitMQ user created and permissions set."
