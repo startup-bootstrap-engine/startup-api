7455806b2fc92fbf29c93a58fa68b5a478e9feee
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-09-04 00:09:58 -0700

    refactor: Add SocketMessaging to MessagingBrokerHandlers constructor

--- src/providers/microservice/messaging-broker/MessagingBrokerHandlers.ts
+++ src/providers/microservice/messaging-broker/MessagingBrokerHandlers.ts
@@ -1,19 +1,28 @@
 import { appEnv } from "@providers/config/env";
 import { NPCMovementMoveTowards } from "@providers/npc/movement/NPCMovementMoveTowards";
 import { NPCManager } from "@providers/npc/NPCManager";
+import { SocketMessaging } from "@providers/sockets/SocketMessaging";
 import { provide } from "inversify-binding-decorators";
 
 @provide(MessagingBrokerHandlers)
 export class MessagingBrokerHandlers {
-  constructor(private npcMovementMoveTowards: NPCMovementMoveTowards, private npcManager: NPCManager) {}
+  constructor(
+    private npcMovementMoveTowards: NPCMovementMoveTowards,
+    private npcManager: NPCManager,
+    private socketMessaging: SocketMessaging
+  ) {}
 
   public async onAddHandlers(): Promise<void> {
-    console.log("💌 Adding messaging broker handlers...");
+    const { MICROSERVICE_NAME, IS_MICROSERVICE } = appEnv.general;
 
-    const { MICROSERVICE_NAME } = appEnv.general;
+    if (!IS_MICROSERVICE) {
+      await this.socketMessaging.addSendEventToUserListener();
+    }
 
     switch (MICROSERVICE_NAME) {
       case "rpg-npc":
+        console.log("💌 Adding messaging broker handlers...");
+
         await this.npcMovementMoveTowards.addPathfindingResultsListener();
         await this.npcMovementMoveTowards.addLightweightPathfindingResultsListener();
         await this.npcManager.startBehaviorLoopListener();
