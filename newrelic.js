c03afbf30e5a0f98f9c874da687587976dfa35d8
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-09-10 12:31:55 -0700

    chore: Update Dockerfile.prod and newrelic.js for New Relic logging configuration

--- newrelic.js
+++ newrelic.js
@@ -1,5 +1,6 @@
 exports.config = {
   rules: {
+    ignore_url_regexes: ["^OtherTransaction/Message/RabbitMQ/Exchange/Named/rpg_microservices$"],
     ignore: ["^OtherTransaction/Message/RabbitMQ/Exchange/Named/rpg_microservices$"],
   },
 };
