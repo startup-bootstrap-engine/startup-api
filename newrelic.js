f47d0eb626eb6be60cb8afb2cc379921661f7242
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-09-11 21:55:09 -0700

    refactor: Update newrelic.js to remove RabbitMQ monitoring and adjust agent configuration

--- newrelic.js
+++ newrelic.js
@@ -1,14 +1,5 @@
 exports.config = {
-  agent_enabled: true, // Make sure the agent is still enabled.
-  transaction_tracer: {
-    enabled: true,
-    record_sql: "off",
-    explain_threshold: 500,
-    ignore_transactions: ["OtherTransaction/Message/RabbitMQ/Exchange/Named/rpg_microservices"],
-  },
-  messaging: {
-    rabbitmq: false, // Disable RabbitMQ monitoring.
-  },
+  agent_enabled: true, // Make sure the agent is still
   logging: {
     level: "warn",
   },
