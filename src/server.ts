628cb62d09c88a79e77221ba60ddd60f92bf36da
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-09-12 23:26:47 -0700

    fixes

--- src/server.ts
+++ src/server.ts
@@ -74,15 +74,11 @@ async function initializeServerComponents(): Promise<void> {
 
   const redisPubSub = container.get(RedisPubSub);
 
-  await Promise.all([
-    database.initialize(),
-    redisManager.connect(),
-    socketAdapter.init(appEnv.socket.type),
-    messagingBrokerHandlers.onAddHandlers(),
-    redisPubSub.init(),
-    redisStreams.init(),
-  ]);
+  await Promise.all([database.initialize(), redisManager.connect(), socketAdapter.init(appEnv.socket.type)]);
 
+  await messagingBrokerHandlers.onAddHandlers();
+  await redisPubSub.init();
+  await redisStreams.init();
   // This should happen on rpg-api only. We have to use pub sub because remember the client only connects to rpg-api, not to rpg-npc (and other microservices). So rpg-npc has to send messages to rpg-api through redis pub sub
   await redisPubSubListeners.addSubscribers();
   await redisStreamsListeners.addSubscribers();
