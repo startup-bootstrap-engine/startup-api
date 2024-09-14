6c41d0d75571c805ff5aa2a3c7f4c71cf5debabc
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-09-13 23:58:43 -0700

    refactor: Adjust NPC raid triggering chance and cron schedules

--- src/providers/cronjobs/NPCCrons.ts
+++ src/providers/cronjobs/NPCCrons.ts
@@ -32,12 +32,12 @@ export class NPCCrons {
       await this.npcSpawnCron();
     });
 
-    this.cronJobScheduler.uniqueSchedule("npc-raid-activator", "*/55 * * * *", async () => {
+    this.cronJobScheduler.uniqueSchedule("npc-raid-activator", "0 */2 * * *", async () => {
       console.log("Trying to activate raids...");
       await this.npcRaidActivator.activateRaids();
     });
 
-    this.cronJobScheduler.uniqueSchedule("npc-raid-shutdown", "*/3 * * * *", async () => {
+    this.cronJobScheduler.uniqueSchedule("npc-raid-shutdown", "* * * * *", async () => {
       await this.npcRaidActivator.shutdownRaids();
     });
     this.cronJobScheduler.uniqueSchedule("npc-freezer", "* * * * *", async () => {
