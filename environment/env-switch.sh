b4c1538cebfc84183bf40b975f4657de14edd3d0
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-10-04 06:58:47 -0700

    finished

--- environment/env-switch.sh
+++ environment/env-switch.sh
@@ -4,8 +4,8 @@ case $1 in
 "dev")
   echo "Generating Development environment .env files"
   cp -fr ./environment/dev.env .env
-  cp -fr ./environment/docker-compose.dev.yml docker-compose.yml
   cp -fr ./environment/Dockerfile.dev Dockerfile
+  yarn module:build
   ;;
 
 "prod")
