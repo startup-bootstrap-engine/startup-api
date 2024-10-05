ed00329e6febc2648e514ce1b0184ccdacc3f53a
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-10-04 21:21:33 -0700

    refactor: Update RepositoryFactory to support Mongoose Model and improve code readability

--- src/providers/database/repository/RepositoryFactory.ts
+++ src/providers/database/repository/RepositoryFactory.ts
@@ -1,6 +1,7 @@
 import { appEnv } from "@providers/config/env";
 import { provide } from "inversify-binding-decorators";
 import { ObjectSchema } from "joi";
+import { Model } from "mongoose";
 import { DatabaseAdaptersAvailable, IRepositoryAdapter } from "../DatabaseTypes";
 import { FirebaseRepository } from "./FirebaseRepository";
 import { MongooseRepository } from "./MongooseRepository";
@@ -10,16 +11,16 @@ export class RepositoryFactory {
   constructor(private firebaseRepository: FirebaseRepository<any>) {}
 
   public createRepository<T extends { [key: string]: any }>(
-    model: any,
+    model: Model<any> | string,
     schema: ObjectSchema,
     type: DatabaseAdaptersAvailable = appEnv.database.DB_ADAPTER as DatabaseAdaptersAvailable
   ): IRepositoryAdapter<T> {
     switch (type) {
       case "mongoose":
-        return new MongooseRepository(model) as unknown as IRepositoryAdapter<T>;
+        return new MongooseRepository(model as Model<any>);
       case "firebase":
         // Ensure model is a string for Firebase
-        this.firebaseRepository.init(model, schema);
+        this.firebaseRepository.init(model as string, schema);
         return this.firebaseRepository;
 
       default:
