a34e83142964aa7e036e0d78b3e9a636d9d40869
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-10-31 07:40:01 -0700

    refactor: simplify UserUpdateDTO by removing unused properties and imports

--- src/useCases/ModuleSystem/user/UserDTO.ts
+++ src/useCases/ModuleSystem/user/UserDTO.ts
@@ -1,34 +1,10 @@
-import { UserExperience, UserGoal } from "@startup-engine/shared/dist";
-import { Type } from "class-transformer";
-import { IsEnum, IsNumber, IsOptional, IsString, ValidateNested } from "class-validator";
-import { tsDefaultDecorator, tsEnumDecorator } from "../../../providers/constants/ValidationConstants";
-
-class UserPreferencesDTO {
-  @IsOptional()
-  @IsEnum(UserExperience, tsEnumDecorator("validation", "isEnum", UserExperience))
-  experience: UserExperience;
-
-  @IsOptional()
-  @IsEnum(UserGoal, tsEnumDecorator("validation", "isEnum", UserGoal))
-  goal: UserGoal;
-}
-
-class UserWalletDTO {
-  @IsString()
-  publicAddress: string;
-
-  @IsNumber()
-  networkId: number;
-}
+import { IsOptional, IsString } from "class-validator";
+import { tsDefaultDecorator } from "../../../providers/constants/ValidationConstants";
 
 export class UserUpdateDTO {
   @IsOptional()
   @IsString(tsDefaultDecorator("validation", "isType", { type: "string" }))
   name: string;
-
-  @IsOptional()
-  @IsString(tsDefaultDecorator("validation", "isType", { type: "string" }))
-  email: string;
 
   @IsOptional()
   @IsString(tsDefaultDecorator("validation", "isType", { type: "string" }))
@@ -41,14 +17,4 @@ export class UserUpdateDTO {
   @IsOptional()
   @IsString(tsDefaultDecorator("validation", "isType", { type: "string" }))
   pushNotificationToken: string;
-
-  @IsOptional()
-  @ValidateNested()
-  @Type(() => UserPreferencesDTO)
-  preferences: UserPreferencesDTO;
-
-  @IsOptional()
-  @ValidateNested()
-  @Type(() => UserWalletDTO)
-  wallet: UserWalletDTO;
 }
